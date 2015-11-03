ba481fb44cd00a23ebe3c84b3892f35891fa9335
matthew-brean <matthew.brean@ocsbstudent.ca>
2015-11-03 14:42:28 -0500

    final

---
+++ LevelToPercent/LTP.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace LevelToPercent
+{
+    partial class frmLTP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblPercent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Location = new System.Drawing.Point(96, 12);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(100, 20);
+            this.txtLevel.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(107, 51);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblPercent
+            // 
+            this.lblPercent.AutoSize = true;
+            this.lblPercent.Location = new System.Drawing.Point(104, 99);
+            this.lblPercent.Name = "lblPercent";
+            this.lblPercent.Size = new System.Drawing.Size(0, 13);
+            this.lblPercent.TabIndex = 2;
+            // 
+            // frmLTP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblPercent);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtLevel);
+            this.Name = "frmLTP";
+            this.Text = "LevelToPercent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblPercent;
+    }
+}
+
---
+++ LevelToPercent/LTP.cs
@@ -0,0 +1,109 @@
+﻿/*
+Created by: Matthew Brean
+Created on: 2015-11-03
+Created for: ICS3U
+Daily Assignment: Unit #4-04
+This program converts level mark to percentage mark.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LevelToPercent
+{
+    public partial class frmLTP : Form
+
+    {
+
+        public int MarkCalculator (string levelMark)
+        {
+            //this function converts level mark to percentage mark
+            int PercentageMark;
+
+            if (levelMark == "4+")
+            {
+                PercentageMark = 95;
+            }
+            else if (levelMark == "4")
+            {
+                PercentageMark = 90;
+            }
+            else if (levelMark == "4-")
+            {
+                PercentageMark = 85;
+            }
+            else if (levelMark == "3+")
+            {
+                PercentageMark = 77;
+            }
+            else if (levelMark == "3")
+            {
+                PercentageMark = 75;
+            }
+            else if (levelMark == "3-")
+            {
+                PercentageMark = 72;
+            }
+            else if (levelMark == "2+")
+            {
+                PercentageMark = 67;
+            }
+            else if (levelMark == "2")
+            {
+                PercentageMark = 65;
+            }
+            else if (levelMark == "2-")
+            {
+                PercentageMark = 62;
+            }
+            else if (levelMark == "1+")
+            {
+                PercentageMark = 57;
+            }
+            else if (levelMark == "1")
+            {
+                PercentageMark = 55;
+            }
+            else if (levelMark == "1-")
+            {
+                PercentageMark = 52;
+            }
+            else if (levelMark == "R")
+            {
+                PercentageMark = 30;
+            }
+            else if (levelMark == "NE")
+            {
+                PercentageMark = 0;
+            }
+
+            else PercentageMark = -1;  
+
+            return PercentageMark;
+        }
+
+
+        public frmLTP()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //variables
+            string levelMark = this.txtLevel.Text;
+            int percentageMark;
+
+           percentageMark = MarkCalculator(levelMark);
+            this.lblPercent.Text = "Your mark is: " + percentageMark;
+            
+        }
+    }
+}
